d3707ada8dbda12eea32f86158f102d8a4aba047
Casey Takeda <kaze@berkeley.edu>
2016-04-10 19:51:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857", "#01FF70", "#F012BE", "#FF851B", "#111111", "#001f3f", ];
 var currentQuote = '', currentAuthor = '';
+var temp = -1;
+var color = -1;
 
 function openURL(url) {
    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
@@ -37,8 +39,11 @@ function getQuote() {
           }, 500);
         $('#author').html(r.author);
       });
-      
-      var color = Math.floor(Math.random() * colors.length);
+      temp = Math.floor(Math.random() * colors.length);
+      while (temp == color) {
+         temp = Math.floor(Math.random() * colors.length);
+      }
+      color = temp;
       $("html body").animate({
         backgroundColor: colors[color],
         color: colors[color]
